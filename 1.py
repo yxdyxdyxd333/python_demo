aa9661a8ccbdb69c8274318b28a738b9df835514
yxdyxdyxd333 <535333905@qq.com>
2021-01-12 14:58:21 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+#格式预处理
+print('''
+换行
+换行
+''')
+#基础数据格式
+a='111'
+b='222'
+print(a+b)
+print(a, b)
+print('show{}, show{}'.format(a,b))
+#name = input('请输入姓名：')
+#age = input('请输入年龄')
+#print('你叫{}，今年{}岁了'.format(a,b))
+c=3.1415926
+print('{:.2f}'.format(c))
+if a == '111':
+    print('a=111')
+else:
+    print('a!=111')
+d = ['星期一','星期二','星期三']
+for i in d:
+    print(i)
+for i in range(5):
+    print(i)
+for i in range(8,9):
+    print(i)
+e = 1
+while e <= 10:
+    print('出走的{}天'.format(e))
+    e += 1
+#逢7必过
+for i in range(100):
+    if i == 7 or i % 10 == 7 or i // 10 == 7:
+        print(i)
